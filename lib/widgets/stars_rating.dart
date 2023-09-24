6cc6a1397917bd472056d636ff064cca04a7f8bb
Harley Mamalias <harleymamalias@gmail.com>
2023-09-24 16:18:10 +0800

    created stars rating widget

---
+++ lib/widgets/stars_rating.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StarsWidget extends StatelessWidget {
+  const StarsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 20,
+      height: 20,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(),
+      child: const Icon(
+        Icons.star,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
